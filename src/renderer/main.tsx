181c9b15e7f707711bf1962811d99d3ee0ceaada
CyberArcenal <148480765+CyberArcenal@users.noreply.github.com>
2026-06-02 04:54:00 +0800

    fix

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -10,14 +10,11 @@ import "reflect-metadata";
 import React from 'react';
 import ConditionalRouter from './components/Shared/ConditionalRouter';
 import App from './routes/App';
-import { SettingsProvider } from './contexts/SettingsContext'; // adjust path
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SettingsProvider>
       <ConditionalRouter>
         <App />
       </ConditionalRouter>
-    </SettingsProvider>
   </React.StrictMode>,
 )
